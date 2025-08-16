f8a92ae4acc15a4669959acf5520482c4b2261d3
Joshua Aldridge <joshu459@hotmail.com>
2025-08-16 14:51:22 +1000

    Adding homepage and Components

--- shared-portfolio/src/App.jsx
+++ shared-portfolio/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// Import required libraries
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { Provider } from "@/components/ui/provider"
+
+// Import components
+import Navbar from "./Components/Navbar.jsx";
+
+
+// Import pages
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <React.StrictMode>
+      <Router>
+        <Navbar />
+      </Router>
+    </React.StrictMode>
   )
 }
-
-export default App
---
+++ shared-portfolio/src/Components/Navbar.jsx
@@ -0,0 +1,7 @@
+import * as Chakra from "@chakra-ui/react";
+
+function Navbar() {
+    return(
+        
+    )
+}
