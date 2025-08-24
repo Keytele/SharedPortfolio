61c3959f69763b7e0edb1e1acec9384734dde1a1
Joshua Aldridge <joshu459@hotmail.com>
2025-08-24 15:45:02 +1000

    Removed Chakra & Homepage Started Removed Chakra as we no longer need it and can use Tailwind CSS instead. Homepage has a start, will fix the cosmetics in the future once complete.

--- shared-portfolio/src/Pages/home.jsx
+++ shared-portfolio/src/Pages/home.jsx
@@ -1,6 +1,19 @@
-import { Provider } from "@/components/ui/provider"
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+function Home() {
+    return( 
+        <>
+            <div className="flex-col justify-center items-center px-6 pt-20 lg:px-9">  
+                    <div className="justify-center text-center">
+                    <h1 className="text-5xl">Welcome to our portfolio site - By Aghi & Josh!</h1>
+                </div>
+                <div className="justify-center text-center">
+                    <h3 className="text-2xl mt-10">IT Graduates</h3>
+                </div>
+                <hr className="h-1 mx-auto my-30 bg-gray-500 border-0 rounded-sm"></hr>
+            </div>
 
+            
+        </>
+    ); 
+}
+
+export default Home;
